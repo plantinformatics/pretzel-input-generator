11883ea1e8ad4f46182c93a4eb0cabc07f555b5b
Rad Suchecki <r.suchecki@gmail.com>
2019-05-24 14:22:28 +1000

    Representative transcripts only. Explicit handle close

--- bin/emblparse.py
+++ bin/emblparse.py
@@ -19,31 +19,39 @@ parser.add_argument('-c', '--chromosome', help='Chromosome name', type=str, requ
 
 
 args = parser.parse_args()
+f = args.infile
 of = args.outfile
 
 # eprint(args.chromosome)
 # eprint(args.assembly)
 # exit(0)
 
-for record in SeqIO.parse(args.infile, "embl"):
+for record in SeqIO.parse(f, "embl"):
   eprint(record.id)
   eprint("Record {} has {} features".format(record.id, len(record.features)))
   for feature in record.features:
     if feature.type == 'gene':
       geneId = feature.qualifiers['locus_tag'][0]
       coords = feature.location
+
       # eprint(coords)
       # eprint(feature)
+
     if feature.type == 'mRNA':
       transcriptId = feature.qualifiers['locus_tag'][0]
       # eprint(feature)
-    if feature.type == 'CDS' and 'translation' in feature.qualifiers:
+      representative = True if 'is_repr=1' in feature.qualifiers['note'] else False
+      # eprint("repr:")
+      # eprint(representative)
+      # exit(0)
+    if feature.type == 'CDS' and representative and 'translation' in feature.qualifiers:
       # eprint(feature)
       aa = feature.qualifiers['translation'][0]
       of.write(">{0} pep chromosome:{1}:{2}:{3}:{4} gene:{5}\n".format(transcriptId,args.assembly,args.chromosome,coords.start,coords.end,geneId))
       of.write("{}\n".format(aa))
 
-
+f.close()
+of.close()
       # break
 
 #TRIDC1AG000120.1 pep chromosome:WEWseq_PGSB_20160501:1A:616966:619040 gene:TRIDC1AG000120
