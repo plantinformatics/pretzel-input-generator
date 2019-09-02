b51d1ae84fe769fdc347ca17f793b5a8a94be9be
Rad Suchecki <r.suchecki@gmail.com>
2019-09-02 10:44:44 +1000

    disallow mismatches at 3' end of aligned marker probe

--- templates/paf2pretzel.groovy
+++ templates/paf2pretzel.groovy
@@ -19,37 +19,53 @@ annotation.meta.shortName = "${meta.markers.name}"
 
 TreeMap scope = [:] //keep keys sorted as the corresponding blocks get displayed in order in pretzel
 pafContent.eachLine { line ->
-  if(!line.startsWith('@')) {
-
     def arr = line.split('\t')
     //NOTE!!! 0-based coordinates, half-open i.e. first position included, last position excluded, or "<a,b)"
     (QNAME, QLEN, QSTART, QEND, STRAND, TNAME, TLEN, TSTART, TEND, MATCHES, ALNLEN, MAPQ) = arr[0..11] //First 12 are positional fields
     def TAGS = arr[12..-1] //variable number of key-value tag pairs
 
     // int qlen = QLEN.toInteger()
-    // int qstart = QSTART.toInteger() + 1
-    // int qend = QEND.toInteger()
-    int tstart = TSTART.toInteger() + 1
+    int qstart = QSTART.toInteger()
+    int qend = QEND.toInteger()
+    int tstart = TSTART.toInteger()
     int tend = TEND.toInteger()
 
-    // int matches = MATCHES.toInteger()
+    int matches = MATCHES.toInteger()
     // int alnlen = ALNLEN.toInteger()
-    double identity = MATCHES.toInteger() / ALNLEN.toInteger()
+    // double aligned_identity = matches / ALNLEN.toInteger()
+    double query_identity = matches / QLEN.toInteger()
+    double query_coverage =  (qend-qstart)/ QLEN.toInteger()
     // int mapq = MAPQ.toInteger()
 
-    if(identity >= min_identity) {
-      def key = TNAME.replaceFirst("^(C|c)(H|h)(R|r)[_]?","")
-      if(!scope.containsKey(key)) {
-        scope << [(key) : []]
+    if(query_identity >= min_identity) {
+      def kosher = true;
+      if(query_identity < 1) { //Not a 100% match, check if any MM in last 3 bases
+        TAGS.each { tag ->
+          if(tag.startsWith('cs:Z')) {
+            if(STRAND == '-' && (tag =~ /^cs:Z:=[acgtnACGTN]{3,}/).count == 0){
+              // println "minus 3'\t"+line
+              kosher = false;
+            } else if(STRAND == '+' && (tag.substring(tag.lastIndexOf('=')+1).matches('^[acgtnACGTN]{3,}\$')) == false) {
+              // println "plus 3'\t"+line
+              kosher = false;
+            }
+          }
+        }
       }
-      scope[key] << [
-        "name" : QNAME,
-        "value" : [ tstart, tend ],
-        "identity" : matches/alnlen
-      ]
+      if(kosher) {
+        def key = TNAME.replaceFirst("^(C|c)(H|h)(R|r)[_]?","")
+        if(!scope.containsKey(key)) {
+          scope << [(key) : []]
+        }
+        scope[key] << [
+          "name" : QNAME,
+          "value" : [ tstart+1, tend ],
+          "identity" : query_identity,
+          "coverage" : query_coverage
+        ]
+      }
     }
     // scope[key] << ["name" : QNAME, "value" : [ alnStart, alnEnd ]]
-  }
 }
 //GROUP TOGETHER FEATURES FROM/IN SAME BLOCK
 annotation.blocks = []
@@ -66,4 +82,4 @@ counts.withWriterAppend{ wr ->
   }
 }
 out.text = prettyPrint(toJson(annotation))
-'gzip ${tag}_markers.json'.execute()
+('gzip ${tag}_markers.json'.execute()).waitFor()
